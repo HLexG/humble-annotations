8960a3d6bdcaa94a69bdcbfc5fb4c61e9785c3da
Shivas Jayaram <shivasj@gmail.com>
2021-03-01 18:54:26 -0500

    Annotations frontend initial files

---
+++ annotations-frontend/docker-shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# Read the settings file
+source ./env.dev
+
+docker build -t $IMAGE_NAME -f Dockerfile.dev .
+docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3000:3000 $IMAGE_NAME
