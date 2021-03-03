ef45252a528d02242e3072f597ef352cd8f3e1b5
Shivas Jayaram <shivasj@gmail.com>
2021-03-03 14:20:31 -0500

    Added get / get details route for documents

--- api-service/dataaccess/documents.py
+++ api-service/dataaccess/documents.py
@@ -3,3 +3,57 @@ from typing import Any, Dict, List
 
 from dataaccess.session import database
 from dataaccess.errors import RecordNotFoundError
+
+async def browse(
+    *,
+    dataset_id: int,
+    page_number: int = 0,
+    page_size: int = 20
+) -> List[Dict[str, Any]]:
+    """
+    Retrieve a list of rows based on filters
+    """
+    
+    query = """
+        select id,dataset_id,filepath 
+        from documents
+        where dataset_id = :dataset_id
+    """
+
+    values = {
+        "dataset_id": dataset_id
+    }
+
+    print("query",query)
+    result = await database.fetch_all(query, values)
+
+    return [prep_data(row) for row in result]
+
+async def get(id: int) -> Dict[str, Any]:
+    """
+    Retrieve one row based by its id. Return object is a dict. 
+    Raises if the record was not found.
+    """
+
+    query = """
+        select id,dataset_id,filepath from documents where id = :id
+    """
+
+    values = {
+        "id": id
+    }
+
+    print("query:",query, "values:", values)
+    result = await database.fetch_one(query, values)
+
+    if result is None:
+        raise RecordNotFoundError(f"Could not find row with id '{id}'")
+
+    return prep_data(result)
+
+def prep_data(result) -> Dict[str, Any]:
+    if result is None:
+        raise ValueError("Tried to prepare null result")
+
+    result = dict(result)
+    return result
