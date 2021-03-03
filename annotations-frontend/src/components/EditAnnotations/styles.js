52e9ec3655ec802897eaf220ec16c62a8df84060
Shivas Jayaram <shivasj@gmail.com>
2021-03-03 17:58:28 -0500

    Template for EditAnnotations

---
+++ annotations-frontend/src/components/EditAnnotations/styles.js
@@ -0,0 +1,20 @@
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        minHeight: "100vh"
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    main: {
+        backgroundColor: "#efedf5",
+        paddingLeft: theme.spacing(3),
+        paddingRight: theme.spacing(3),
+        paddingBottom: theme.spacing(2),
+        paddingTop: 15,
+        zIndex: theme.zIndex.drawer + 1,
+        minHeight: "100vh"
+    },
+});
+
+export default styles;
