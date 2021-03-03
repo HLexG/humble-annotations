589bb9843c195b670abcdc7b24fdf719a5e0a8ca
Shivas Jayaram <shivasj@gmail.com>
2021-03-03 07:47:31 -0500

    Added /datasets to routes

--- api-service/api/routers/datasets.py
+++ api-service/api/routers/datasets.py
@@ -5,4 +5,15 @@ from fastapi import APIRouter, Path, Query
 from starlette.responses import FileResponse
 from urllib.parse import urlparse
 
+from dataaccess import datasets
+
 router = APIRouter()
+
+@router.get(
+    "/datasets",
+    summary="Get list of datasets",
+    description="Get list of datasets"
+)
+async def datasets_index():
+    
+    return await datasets.browse()
