b8d01477375bf7d57bbbde42a06e130ecafa16f9
Shivas Jayaram <shivasj@gmail.com>
2021-03-03 08:30:06 -0500

    Added --network

--- api-service/docker-shell.sh
+++ api-service/docker-shell.sh
@@ -5,5 +5,8 @@ set -e
 # Load the environment file
 source ./env.dev
 
+# Create the network if we don't have it yet
+docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
+
 docker build -t $IMAGE_NAME -f Dockerfile .
-docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9000:9000 -e DEV=1 $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9000:9000 -e DEV=1 --network hlexg $IMAGE_NAME
