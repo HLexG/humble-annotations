2c8836af2bb20d24e565f5b15c6622a3e6db1484
Shivas Jayaram <shivasj@gmail.com>
2021-03-03 16:21:23 -0500

    Dataservice for backend access

---
+++ annotations-frontend/src/services/DataServices.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const BASE_API_URL = 'http://0.0.0.0:9000/v1';
+
+//axios.defaults.baseURL = BASE_API_URL;
+
+
+const DataServices = {
+    Init: function(){
+        // Any application initialization logic comes here
+    }
+}
+
+export default DataServices;
