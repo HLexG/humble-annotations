1312427f1c5353e50483480b73f6c76424c420c3
Shivas Jayaram <shivasj@gmail.com>
2021-03-04 18:53:43 -0500

    Loading document details based on url params

--- annotations-frontend/src/components/EditAnnotations/index.js
+++ annotations-frontend/src/components/EditAnnotations/index.js
@@ -2,16 +2,36 @@ import React, {useEffect, useRef, useState} from 'react';
 import { withStyles } from '@material-ui/core';
 
 import Annotator from '../Annotator';
+import DataServices from "../../services/DataServices";
 import styles from './styles';
 
 const EditAnnotations = ( props ) => {
     const {classes} = props;
     const { history } = props;
-    //console.log(props)
 
     console.log("================================== EditAnnotations ======================================");
 
     // Component States
+    const [id , setId] = useState(null);
+    const [document , setDocument] = useState(null);
+    const loadDocument = (id) => {
+        DataServices.GetDocument(id)
+            .then(function (response) {
+                setDocument(response.data);
+            })
+    }
+
+    // Setup Component
+    useEffect(() => {
+        if(props.match.params.id){
+            setId(props.match.params.id);
+        }
+      }, []);
+    useEffect(() => {
+        if(id){
+            loadDocument(id);
+        }
+      }, [id]);
 
     return (
         <div className={classes.root}>
