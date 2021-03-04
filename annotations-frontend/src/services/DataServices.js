e233faf3410f57cfb6e7046bb2531839cd6fb5e5
Shivas Jayaram <shivasj@gmail.com>
2021-03-04 16:24:59 -0500

    Get document from api server

--- annotations-frontend/src/services/DataServices.js
+++ annotations-frontend/src/services/DataServices.js
@@ -8,7 +8,16 @@ const BASE_API_URL = 'http://0.0.0.0:9000/v1';
 const DataServices = {
     Init: function(){
         // Any application initialization logic comes here
-    }
+    },
+    GetDatasets : async function(){
+        return await axios.get(BASE_API_URL+"/datasets");
+    },
+    GetDocumentsForAnnotation : async function(dataset_id){
+        return await axios.get(BASE_API_URL+"/documents?dataset_id="+dataset_id);
+    },
+    GetDocument : async function(id){
+        return await axios.get(BASE_API_URL+"/documents/"+id);
+    },
 }
 
 export default DataServices;
