2ee5f78c984a37e89b41e1bbbf73ea1822a5f35a
lalopey <lalopey@gmail.com>
2021-03-04 13:05:27 +0400

    (feat) Added Spacy extractor-2 skeleton code

---
+++ feature-extractor-02/extractor/routers/mentions.py
@@ -0,0 +1,39 @@
+import os
+import io
+#from pathlib import Path
+import pandas as pd
+from glob import glob
+from fastapi import APIRouter, Path, Query
+from starlette.responses import FileResponse
+from urllib.parse import urlparse
+
+import spacy
+
+router = APIRouter()
+
+# Initialize model here
+# Load pretrained model
+# any other pre model step
+
+#model = ...
+
+@router.post(
+    "/find_mentions",
+    summary="Find mentions in given text",
+    description="Find mentions in given text"
+)
+async def find_mentions(
+        input: dict
+):
+    print("find_mentions")
+    print("input", input)
+
+    # Find mentions
+    #model_output = model...
+
+    # Format output mentions /clusters
+    
+
+    return {
+        "response": {'test':'testing','output':input}
+    }
