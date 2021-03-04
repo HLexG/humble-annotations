0d276448ae2465954a7f7eeeab44d25c1c800a95
Shivas Jayaram <shivasj@gmail.com>
2021-03-03 20:34:19 -0500

    Added template structure for dashboard component

--- annotations-frontend/src/components/Home/index.js
+++ annotations-frontend/src/components/Home/index.js
@@ -1,5 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { withStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 import DataServices from "../../services/DataServices";
 import styles from './styles';
@@ -13,7 +17,64 @@ const Home = ( props ) => {
     return (
         <div className={classes.root}>
             <main className={classes.main}>
-                Home Page Content...
+                <Grid container spacing={2}>
+                    <Grid item sm={4}>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    # Annotations Completed
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Some metrics here....
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    # Annotations Completed
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Some metrics here....
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item sm={4}>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    # Annotations Completed
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Some metrics here....
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    # Annotations Completed
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Some metrics here....
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item sm={4}>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Annotations Tasks
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Some metrics here....
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </main>
         </div>
     );
