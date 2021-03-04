7bb93790b59ad0f11fcb4f44ab0cd5cb09937ccd
lalopey <lalopey@gmail.com>
2021-03-04 12:19:51 +0400

    (feat) Implemented mention extraction with SpanBERT in routers

--- feature-extractor-01/extractor/routers/mentions.py
+++ feature-extractor-01/extractor/routers/mentions.py
@@ -1,17 +1,28 @@
 import os
 import io
+#from pathlib import Path
 import pandas as pd
 from glob import glob
 from fastapi import APIRouter, Path, Query
 from starlette.responses import FileResponse
 from urllib.parse import urlparse
 
+from extractor.routers.utils import download_file
+
+import sys
+sys.path.append("extractor/models/spanbert")
+
+from inference.inference import Inference
+
 router = APIRouter()
 
 # Initialize model here
 # Load pretrained model
 # any other pre model step
-model = None
+
+download_file("https://storage.googleapis.com/hlexg/models/spanbert_model.pth", base_path="extractor/models/spanbert")
+
+model = Inference("extractor/models/spanbert/spanbert_model.pth")
 
 @router.post(
     "/find_mentions",
@@ -25,11 +36,12 @@ async def find_mentions(
     print("input", input)
 
     # Find mentions
-    #mentions = model.predict(...)
+    model_output = model.perform_coreference(input['text'])
+    output = model_output["clusters"][0][0][1]
 
     # Format output mentions /clusters
     
 
     return {
-        "response": {'test':'testing','input':input}
+        "response": {'test':'testing','output':output}
     }
