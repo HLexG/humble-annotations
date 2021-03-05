9c998c0d7f4af6377d0a5d2b92c7d3f413b54a9f
Shivas Jayaram <shivasj@gmail.com>
2021-03-05 08:24:03 -0500

    Added toolbar place holder

--- annotations-frontend/src/components/EditAnnotations/styles.js
+++ annotations-frontend/src/components/EditAnnotations/styles.js
@@ -11,9 +11,27 @@ const styles = theme => ({
         paddingLeft: theme.spacing(3),
         paddingRight: theme.spacing(3),
         paddingBottom: theme.spacing(2),
-        paddingTop: 15,
+        paddingTop: 0,
         zIndex: theme.zIndex.drawer + 1,
         minHeight: "100vh"
+    },
+    toolbar:{
+        background: "#ffffff",
+        padding:0,
+        height: 40,
+    },
+    toolbartitle:{
+        fontSize: "12px",
+        fontWeight: 600,
+        color: "#444",
+    },
+    toolbartext:{
+        fontSize: "16px",
+        fontWeight: 300,
+        color: "#444",
+    },
+    toolbaricon:{
+        color: "#444",
     },
 });
 
