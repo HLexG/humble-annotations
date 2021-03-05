6d1ec62da7be850587495791ed265dd0267fdaa5
Shivas Jayaram <shivasj@gmail.com>
2021-03-05 08:39:44 -0500

    Added styles to Annotations component

--- annotations-frontend/src/components/Annotation/styles.js
+++ annotations-frontend/src/components/Annotation/styles.js
@@ -1,10 +1,24 @@
 const styles = theme => ({
-    root: {
-        flexGrow: 1,
-        minHeight: "100vh"
-    },
-    grow: {
-        flexGrow: 1,
+    content: {
+        color: "#444",
+        fontSize: "20px",
+        fontFamily: "Lato, Trebuchet MS, Roboto, Helvetica, Arial, sans-serif",
+        lineHeight: "2.0em",
+        background: "#fff",
+        borderLeft: "1px solid #ddd",
+        borderRight: "1px solid #ddd",
+        borderBottom: "1px solid #ddd",
+        padding: 15,
+        minHeight: "90vh",
+        "& span":{
+            padding: "10px 10px 10px 0px",
+        },
+        "& span span":{
+            padding: "8px 8px 8px 0px",
+        },
+        "& span mark":{
+            padding: "10px 10px 10px 0px",
+        },
     },
 });
 
