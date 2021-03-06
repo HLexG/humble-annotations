55ab2bf57a769d952ee432bebd14896b39d3a75f
Shivas Jayaram <shivasj@gmail.com>
2021-03-05 19:10:21 -0500

    Updated Annotation component

--- annotations-frontend/src/components/Annotation/index.js
+++ annotations-frontend/src/components/Annotation/index.js
@@ -10,6 +10,23 @@ const Annotation = ( props ) => {
     console.log("================================== Annotation ======================================");
 
     // Component States
+    const [annotationTree, setAnnotationTree] = useState(null);
+    const [selectedToken, setSelectedToken] = useState(null);
+
+    // Setup Component
+
+    // Component methods
+    const isTokenSelected = (token) => {
+        var style = {};
+        var selectedStyle = {
+            textDecoration: "underline"
+        }
+        if(selectedToken && (selectedToken.t_id === token.t_id) && (selectedToken.s_id === token.s_id)){
+            style = selectedStyle
+        }
+
+        return style;
+    }
 
     return (
         <div className={classes.content}>
