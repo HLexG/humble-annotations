bffad40e67afe74438e0d2a1bdfb1ec824a97c1f
Shivas Jayaram <shivasj@gmail.com>
2021-03-07 11:19:24 -0500

    Added renderAnnotationItems

--- annotations-frontend/src/components/Annotation/index.js
+++ annotations-frontend/src/components/Annotation/index.js
@@ -2,6 +2,8 @@ import React, {useEffect, useRef, useState} from 'react';
 import { withStyles } from '@material-ui/core';
 
 import styles from './styles';
+import {handleTokenClick,handleMentionClick,
+    handleMentionDragStart, handleMentionDragOver, handleMentionDrop} from './handlers';
 
 const Annotation = ( props ) => {
     const {classes} = props;
@@ -13,25 +15,92 @@ const Annotation = ( props ) => {
     const [annotationTree, setAnnotationTree] = useState(null);
     const [selectedToken, setSelectedToken] = useState(null);
 
-    // Setup Component
+    // State holder for reference in handlers
+    let state = {
+        // "tokens": tokens,
+        // "setTokens": setTokens,
+        // "annotations": annotations,
+        // "setAnnotations": setAnnotations,
+        // "selectedToken":selectedToken,
+        // "setSelectedToken":setSelectedToken,
+        // "selectedMention":selectedMention,
+        // "setSelectedMention":setSelectedMention,
+        // "draggedMention":draggedMention,
+        // "setDraggedMention":setDraggedMention,
+        // "refresh":refresh,
+        // "setRefresh":setRefresh
+    }
 
-    // Component methods
+    // Setup Component
+    useEffect(() => {
+        //console.log(props.annotationTree);
+        // Set state from props
+        setAnnotationTree(props.annotationTree);
+
+      }, []);
+
+    // Component functions
     const isTokenSelected = (token) => {
         var style = {};
         var selectedStyle = {
             textDecoration: "underline"
         }
-        if(selectedToken && (selectedToken.t_id === token.t_id) && (selectedToken.s_id === token.s_id)){
+        if(selectedToken && (selectedToken.token_id === token.token_id) && (selectedToken.sentence_id === token.sentence_id)){
             style = selectedStyle
         }
-
         return style;
     }
+    const renderAnnotationItems = (items) => {
+        return (
+            <>
+                { items.map(i => {
+                    if(i.type == "token"){
+                        let className = classes.token;
+                        let style = isTokenSelected(i.obj);
+                        let onClick = (event)=>handleTokenClick(event,i.obj,false,state);
+
+                        // Styles for special character tokens
+                        if([',','.',';'].includes(i.obj.text)){
+                            style["whiteSpace"] = "normal";
+                            style["marginLeft"] = "-2px";
+                        }
+                        return (
+                            <a 
+                                key={i.id} 
+                                className={className}
+                                onClick={onClick} 
+                                style={style}
+                            >
+                                {i.obj.text}
+                            </a> 
+                        )
+                    }else{
+                        return (
+                            <span 
+                                key={i.id} 
+                                className={classes.mention}
+                                draggable={true}
+                                onClick={(event)=>handleMentionClick(event,i.obj,state)} 
+                                onDragStart={(event)=>handleMentionDragStart(event,i.obj,state)}
+                                onDragOver={(event)=>handleMentionDragOver(event)}
+                                onDrop={(event)=>handleMentionDrop(event,i.obj,state)}
+                            >
+                                <mark className={classes.mentionhead}><a className={classes.mentionheadtext}>{ i.obj.text }</a></mark>
+                                { i.nodes && renderAnnotationItems(i.nodes) }
+                            </span>
+                        )
+                    }
+                    
+                })}
+            </>
+        )
+        }
 
     return (
         <div className={classes.content}>
-            Annotation....
-            
+            {annotationTree && (
+                renderAnnotationItems(annotationTree.nodes)
+            )}
         </div>
     );
 };
