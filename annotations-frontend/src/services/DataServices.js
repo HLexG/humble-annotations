45e7fd61f1f70a86a128034b880f334e80988482
Shivas Jayaram <shivasj@gmail.com>
2021-03-08 17:55:05 -0500

    Added call to find mentions in spanbert

--- annotations-frontend/src/services/DataServices.js
+++ annotations-frontend/src/services/DataServices.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const BASE_API_URL = 'http://0.0.0.0:9000/v1';
+const BASE_FEATURE_EXTRACTOR01_URL = "http://0.0.0.0:9010/v1";
 
 //axios.defaults.baseURL = BASE_API_URL;
 
@@ -17,6 +18,9 @@ const DataServices = {
     },
     GetDocument : async function(id){
         return await axios.get(BASE_API_URL+"/documents/"+id);
+    },
+    FeatureExtractor01FindMentions: async function(text){
+        return await axios.post(BASE_FEATURE_EXTRACTOR01_URL+"/find_mentions",{'text':text});
     },
 }
 
