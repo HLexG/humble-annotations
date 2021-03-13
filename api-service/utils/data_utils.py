d3bb76683d25a1bf32fff69cd0f07d208e0a097b
lalopey <lalopey@gmail.com>
2021-03-13 23:26:56 +0400

    Progress on entitylink insert to db

--- api-service/utils/data_utils.py
+++ api-service/utils/data_utils.py
@@ -1,13 +1,15 @@
-
 import threading
 from queue import Queue
 
-from dataaccess import datasets, documents
+import pandas as pd
+
+from dataaccess import entitylink
 
 thread_count = 50
 threads = []
 # Initial queue
 queue = Queue(0)
+
 
 class ProcessorThread(threading.Thread):
     def __init__(self, queue, thread):
@@ -23,6 +25,7 @@ class ProcessorThread(threading.Thread):
 
             self.queue.task_done()
 
+
 def download_from_wiki(url,img_dir,file_path):
     try:
         ...
@@ -37,24 +40,34 @@ def download_from_wiki(url,img_dir,file_path):
         print("Error in url:", url)
         print(e)
 
+
 async def load_entity_links(dataset_id):
     print("Loading Entity Links...")
 
     # Dataset
-    dataset = await datasets.get(dataset_id)
+    # dataset = await datasets.get(dataset_id)
 
-    # Read documents for dataset_id
-    #documents = await documents.browse(dataset_id=dataset_id)
-
-    # For each document parse text and find entities
-
-    # Find suggestive entity links for each entities
+    # Load csv file with entity linking database
+    df = pd.read_csv('data/entities.csv')
 
     # Delete all rows from entitylinks
+    entitylink.delete_all(dataset_id)
 
     # Create entitylinks
+    for row in df.iterrows():
+        print(row)
+        x = await entitylink.create(alt_id=row[1]['wiki_id'],
+                                    dataset_id=dataset_id,
+                                    entity_name=row[1]['name'],
+                                    description=row[1]['description'],
+                                    url=row[1]['url'],
+                                    id=row[1]['qid'])
 
 
+
+
+
+    """
     count = 0
     for url in range(75):
         print(url)
@@ -68,6 +81,7 @@ async def load_entity_links(dataset_id):
         threads.append(thread)
     for thread in threads:
         thread.join()
+    """
     
     print("Complete....")
-    
+
