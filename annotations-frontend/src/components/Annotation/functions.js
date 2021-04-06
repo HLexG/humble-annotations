348fabdadbe74da901802609b8d366e8a6e3f6af
Shivas Jayaram <shivasj@gmail.com>
2021-04-06 07:44:53 -0400

    Frontend changes for color map

--- annotations-frontend/src/components/Annotation/functions.js
+++ annotations-frontend/src/components/Annotation/functions.js
@@ -3,7 +3,7 @@ export const buildAnnotationTree = (tokens, annotations) => {
     if(tokens && tokens.length > 0){
         // Create Cluster index look up
         let clusters = {};
-        annotations["clusters"].forEach((cluster) => {
+        annotations["clusters"].forEach((cluster, c_idx) => {
             clusters[cluster["id"]] = cluster;
         });
         console.log(clusters);
@@ -51,4 +51,57 @@ export const buildAnnotationTree = (tokens, annotations) => {
     }else{
         return null;
     }
+}
+
+export const HSLToRGB = (h,s,l) => {
+    // Must be fractions of 1
+    s /= 100;
+    l /= 100;
+  
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2,
+        r = 0,
+        g = 0,
+        b = 0;
+    
+    if (0 <= h && h < 60) {
+        r = c; g = x; b = 0;  
+    } else if (60 <= h && h < 120) {
+        r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+        r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+        r = c; g = 0; b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return [r,g,b];
+}
+
+export const buildColorPalette = (hueStep, saturationStep, lightnessStep) => {
+    if (!hueStep) hueStep = 25;
+    if (!saturationStep) saturationStep = 25;
+    if (!lightnessStep) lightnessStep = 10;
+
+    var color_palette = [];
+    for (var s=100; s>0; s-=saturationStep) {
+        for (var l=80; l>10; l-=lightnessStep) {
+            for (var h=0; h<360; h+=hueStep) {
+                let color = {
+                    "hsl": [h, s, l],
+                    "rgb": HSLToRGB(h, s, l)
+                }
+                color_palette.push(color);
+            }
+        }
+    }
+
+    return color_palette;
 }
