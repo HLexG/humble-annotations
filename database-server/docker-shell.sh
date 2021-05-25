d24e34b518746b842e1122d340ade3b43a7b9a22
Shivas Jayaram <shivasj@gmail.com>
2021-05-25 10:15:27 -0400

    Removed env file

--- database-server/docker-shell.sh
+++ database-server/docker-shell.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-# Read the settings file
-source ./env.dev
-
 # Create the network if we don't have it yet
 docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
 
 # Run Postgres DB and DBMate
-docker-compose run --rm --service-ports --name database-client dbmate
+docker-compose run --rm --service-ports hlexgdb-client
