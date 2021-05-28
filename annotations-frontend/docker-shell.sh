018ee60f7c09287aa370eaabf6852ca657a5c0df
Shivas Jayaram <shivasj@gmail.com>
2021-05-28 08:13:49 -0400

    Changed react app default dev port

--- annotations-frontend/docker-shell.sh
+++ annotations-frontend/docker-shell.sh
@@ -6,4 +6,4 @@ set -e
 source ./env.dev
 
 docker build -t $IMAGE_NAME -f Dockerfile.dev .
-docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3000:3000 $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti -v "$(pwd)/:/app/" -p 3100:3000 $IMAGE_NAME
