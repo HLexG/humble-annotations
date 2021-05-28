d8a2e2ce0bebf4c018e65be8ae1692c69aba65d6
Shivas Jayaram <shivasj@gmail.com>
2021-05-28 10:14:28 -0400

    Added auth login components

---
+++ annotations-frontend/src/components/auth/Login/styles.js
@@ -0,0 +1,20 @@
+const styles = theme => ({
+    root: {
+        flexGrow: 1
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    main: {
+        marginTop : "50px"
+    },
+    inputContainer: {
+        marginTop : "10px",
+    },
+    buttonContainer:{
+        marginTop : "20px",
+        marginBottom : "20px",
+    },
+});
+
+export default styles;
