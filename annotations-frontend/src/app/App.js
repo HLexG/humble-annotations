b2a621636e5f35279b4f1759d3b26239cde7f818
Shivas Jayaram <shivasj@gmail.com>
2021-05-28 09:55:13 -0400

    Refactored Header and content

--- annotations-frontend/src/app/App.js
+++ annotations-frontend/src/app/App.js
@@ -7,27 +7,31 @@ import {
 import './App.css';
 import Theme from "./Theme";
 import AppRoutes from "./AppRoutes";
-import Content from "../common/layout/Content";
-import Header from "../common/layout/Header";
-import DataServices from '../services/DataServices';
+import Content from "../common/Content";
+import Header from "../common/Header";
+import DataService from '../services/DataService';
+import {AuthContextProvider} from '../services/AuthService';
 
 const App = (props) => {
 
   console.log("================================== App ======================================");
 
-  DataServices.Init();
+  // Init Data Service
+  DataService.Init();
 
   // Build App
   let view = (
       <React.Fragment>
           <CssBaseline />
           <ThemeProvider theme={Theme}>
+            <AuthContextProvider>
               <Router basename="/">
                   <Header></Header>
                   <Content>
                       <AppRoutes />
                   </Content>
               </Router>
+            </AuthContextProvider>
           </ThemeProvider>
       </React.Fragment>
   )
