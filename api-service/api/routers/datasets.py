73d411acb68aa0b87b0377b825ad302a656bd1b5
Shivas Jayaram <shivasj@gmail.com>
2021-06-11 10:11:32 -0400

    Added datasets create route

--- api-service/api/routers/datasets.py
+++ api-service/api/routers/datasets.py
@@ -1,22 +1,60 @@
 import os
 import io
 import pandas as pd
-from fastapi import APIRouter, Path, Query
+from fastapi import APIRouter, Path, Query, Depends
 from starlette.responses import FileResponse
 from urllib.parse import urlparse
 
-from dataaccess import datasets
+from api.auth import Auth, OptionalAuth
+from api.data_models import DatasetCreate, DatasetUpdate, Pagination
+from dataaccess import datasets as dataaccess_datasets
+from api.errors import AccessDeniedError
+from dataaccess.types import PermissionType
 
 router = APIRouter()
 
 @router.get(
     "/datasets",
+    tags=["Datasets"],
     summary="Get list of datasets",
     description="Get list of datasets"
 )
-async def datasets_index():
+async def datasets_index(
+    q: str = Query(None, description="An optional search query"),
+    pagination: Pagination = Depends(), 
+    auth: OptionalAuth = Depends()
+):
     
-    return await datasets.browse()
+    return await dataaccess_datasets.browse(
+        q=q,
+        page_number=pagination.page_number,
+        page_size=pagination.page_size
+    )
+
+@router.post(
+    "/datasets",
+    tags=["Datasets"],
+    summary="Create a new dataset",
+    description="Create a new dataset using the provided metadata.",
+    response_description="The created dataset"
+)
+async def datasets_create(
+    dataset: DatasetCreate, 
+    auth: Auth = Depends()
+):
+    # Create dataset
+    dataset_db = await dataaccess_datasets.create(
+        dataset_name=dataset.dataset_name,
+        dataset_description=dataset.dataset_description
+    )
+
+    # Assign the user to the dataset
+    project_user_db = await dataaccess_datasets.create_dataset_user(
+        dataset_id=dataset_db["id"],
+        user_id=auth.user_id,
+        permission_type=PermissionType.owner)
+
+    return dataset_db
 
 @router.get(
     "/datasets/{id}",
@@ -28,6 +66,6 @@ async def datasets_index():
 async def datasets_fetch(
         id: int = Path(..., description="The dataset id")
 ):
-    result = await datasets.get(id)
+    result = await dataaccess_datasets.get(id)
 
     return result
