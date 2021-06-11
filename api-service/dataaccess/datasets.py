c0e18d58d365fce7916f9949d0af27e9dacda214
Shivas Jayaram <shivasj@gmail.com>
2021-06-11 09:14:25 -0400

    Added data access methods for datasets

--- api-service/dataaccess/datasets.py
+++ api-service/dataaccess/datasets.py
@@ -1,11 +1,13 @@
 import os
 from typing import Any, Dict, List
 
+from dataaccess import utils as data_utils
 from dataaccess.session import database
 from dataaccess.errors import RecordNotFoundError
 
 async def browse(
     *,
+    q: str = None,
     page_number: int = 0,
     page_size: int = 20
 ) -> List[Dict[str, Any]]:
@@ -14,10 +16,13 @@ async def browse(
     """
     
     query = """
-        select id,dataset_name from datasets
+        select id,dataset_name ,dataset_description
+        from datasets
+        where 1=1
     """
 
-    values = []
+    if q is not None:
+        query += " and (dataset_name like'%"+q+"%' or dataset_description like '%"+q+"%')"
 
     print("query",query)
     result = await database.fetch_all(query)
@@ -31,7 +36,8 @@ async def get(id: int) -> Dict[str, Any]:
     """
 
     query = """
-        select id,dataset_name from datasets where id = :id
+        select id,dataset_name ,dataset_description
+        from datasets where id = :id
     """
 
     values = {
@@ -45,6 +51,80 @@ async def get(id: int) -> Dict[str, Any]:
         raise RecordNotFoundError(f"Could not find row with id '{id}'")
 
     return prep_data(result)
+
+async def create(*,
+                 dataset_name: str,
+                 dataset_description: str = None,
+                 id: int = None) -> Dict[str, Any]:
+    """
+    Create a new row. Returns the created record as a dict.
+    """
+
+    # Set the values
+    values = {
+        "dataset_name": dataset_name
+    }
+
+    # if the id was passed
+    if id is not None:
+        values["id"] = id
+    if dataset_description is not None:
+        values["dataset_description"] = dataset_description
+
+    # Generate the field and values list
+    field_list = ", ".join(values.keys())
+    param_list = ", ".join(":" + key for key in values.keys())
+
+    result = await database.fetch_one(f"""
+        INSERT INTO datasets (
+            {field_list}
+        ) VALUES (
+            {param_list}
+        ) RETURNING *;
+    """, values=values)
+
+    result = prep_data(result)
+    return result
+
+async def update(id: int,
+                 dataset_name: str = None,
+                 dataset_description: str = None,
+                 ) -> Dict[str, Any]:
+    """
+    Updates an existing row. Keyword arguments left at None will not be
+    changed in the database. Returns the updated record as a dict. Raises if
+    the record was not found.
+    """
+
+    values = {
+        "id": id
+    }
+
+    changes: Dict[str, Any] = {
+    }
+
+    if dataset_name is not None:
+        changes["dataset_name"] = dataset_name
+    if dataset_description is not None:
+        changes["dataset_description"] = dataset_description
+
+    change_list = ", ".join(key + " = :" + key for key in changes.keys())
+    change_list += ", updated_at = EXTRACT(EPOCH FROM clock_timestamp()) * 1000"
+
+    print(change_list)
+
+    result = await database.fetch_one(f"""
+        UPDATE datasets
+        SET {change_list}
+        WHERE id = :id
+        RETURNING *;
+    """, values={**values, **changes})
+
+    if result is None:
+        raise RecordNotFoundError(f"Could not update row with id '{id}'")
+
+    result = prep_data(result)
+    return result
 
 def prep_data(result) -> Dict[str, Any]:
     if result is None:
