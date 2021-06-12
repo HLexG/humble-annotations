bdbc3c40e1e52714c76691d26ef271377c54ee8b
Shivas Jayaram <shivasj@gmail.com>
2021-06-12 11:50:19 -0400

    Save uploaded file

--- api-service/fileaccess/datasets.py
+++ api-service/fileaccess/datasets.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import zipfile
 
 from utils import extract_environment_variable
 
@@ -13,17 +14,22 @@ def is_remote_store():
         return False
 
 def save_extract_dataset(file,dataset_id):
-    file_path = os.path.join(DATASTORE_BUCKET,dataset_id)
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
-    file_path = os.path.join(file_path,str(time.time())+".zip")
+    dataset_path = os.path.join(DATASTORE_BUCKET,dataset_id)
+    if not os.path.exists(dataset_path):
+        os.makedirs(dataset_path)
+    file_path = os.path.join(dataset_path,str(time.time())+".zip")
     # Save the file
     with open(file_path,'wb') as write_file:
         write_file.write(file)
     
     # Extract the zip file
+    with zipfile.ZipFile(file_path) as zfile:
+        # Get a list of all archived file names from the zip
+       document_list = zfile.namelist()
+       zfile.extractall(dataset_path)
 
     # Read all the extracted documents
+    document_list = [os.path.join(dataset_path,path) for path in document_list]
+    print(document_list)
 
-        
-
+    return document_list
