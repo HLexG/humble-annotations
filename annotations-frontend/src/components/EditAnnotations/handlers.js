9594e827c43668e06477c74103172ac335f11566
Shivas Jayaram <shivasj@gmail.com>
2021-06-13 11:45:35 -0400

    Fixed old reference to DataServices

--- annotations-frontend/src/components/EditAnnotations/handlers.js
+++ annotations-frontend/src/components/EditAnnotations/handlers.js
@@ -4,34 +4,34 @@ import {useState} from 'react';
 
 export const handleApplyFeatureExtraction = (state) => {
 
-    if(state["featureExtractor"] == "feature-extractor-01"){
-        // Get annotations from feature extractor
-        DataServices.FeatureExtractor01FindMentions(state["document"]["text"])
-            .then(function (response) {
-                //console.log(response.data);
-                let annotations = response.data;
+    // if(state["featureExtractor"] == "feature-extractor-01"){
+    //     // Get annotations from feature extractor
+    //     DataService.FeatureExtractor01FindMentions(state["document"]["text"])
+    //         .then(function (response) {
+    //             //console.log(response.data);
+    //             let annotations = response.data;
 
-                // Create a mention id
-                //annotations["mentions"].map(mention => mention.cluster_id);
-                //annotations["mentions"] = annotations["mentions"].map((mention,idx) => ({ ...mention, id:idx }));
-                annotations["mentions"].forEach((m, m_idx) => {
-                    m["id"] = m_idx;
-                    if(m["start_token_id"] > m["end_token_id"]){
-                        m["end_token_id"] = m["start_token_id"];
-                    }
-                })
+    //             // Create a mention id
+    //             //annotations["mentions"].map(mention => mention.cluster_id);
+    //             //annotations["mentions"] = annotations["mentions"].map((mention,idx) => ({ ...mention, id:idx }));
+    //             annotations["mentions"].forEach((m, m_idx) => {
+    //                 m["id"] = m_idx;
+    //                 if(m["start_token_id"] > m["end_token_id"]){
+    //                     m["end_token_id"] = m["start_token_id"];
+    //                 }
+    //             })
                 
-                // Get the clusters
-                annotations["clusters"] = [...new Set(annotations["mentions"].map(mention => mention.cluster_id))];
-                annotations["clusters"] = annotations["clusters"].map((cluster,idx) => ({ id:cluster, name: 'M'+cluster }));
-                //console.log(annotations);
+    //             // Get the clusters
+    //             annotations["clusters"] = [...new Set(annotations["mentions"].map(mention => mention.cluster_id))];
+    //             annotations["clusters"] = annotations["clusters"].map((cluster,idx) => ({ id:cluster, name: 'M'+cluster }));
+    //             //console.log(annotations);
 
-                state["setAnnotations"](annotations);
-            })
-    }
-    if(state["featureExtractor"] == "feature-extractor-02"){
+    //             state["setAnnotations"](annotations);
+    //         })
+    // }
+    // if(state["featureExtractor"] == "feature-extractor-02"){
 
-    }
+    // }
 
 
     state["setOpenFeatureExtractorDialog"](false);
