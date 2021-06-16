d4511787cec46ecc4177ed082db00689373bd650
lalopey <lalopey@gmail.com>
2021-06-16 22:01:00 +0400

    (feat) Updated SpanBert extractor to process all docs

--- feature-extractor-01/docker-shell.sh
+++ feature-extractor-01/docker-shell.sh
@@ -5,5 +5,8 @@ set -e
 # Load the environment file
 source ./env.dev
 
+# Create the network if we don't have it yet
+docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
+
 docker build -t $IMAGE_NAME -f Dockerfile .
-docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9010:9010 -e DEV=1 $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9110:9010 -e DEV=1 --network hlexg $IMAGE_NAME
--- feature-extractor-01/extractor/routers/mentions.py
+++ feature-extractor-01/extractor/routers/mentions.py
@@ -7,7 +7,8 @@ from fastapi import APIRouter, Path, Query
 #from starlette.responses import FileResponse
 #from urllib.parse import urlparse
 
-from extractor.routers.utils import download_file, process_clusters
+from extractor import processor
+from extractor.routers.utils import download_file
 
 import sys
 sys.path.append("extractor/models/spanbert")
@@ -21,6 +22,7 @@ download_file("https://storage.googleapis.com/hlexg/models/spanbert_model.pth", 
 # Initialize SpanBERT model (this step will download SpanBERT from Huggingface)
 model = Inference("extractor/models/spanbert/spanbert_model.pth")
 
+
 @router.post(
     "/find_mentions",
     summary="Find mentions in given text",
@@ -29,14 +31,20 @@ model = Inference("extractor/models/spanbert/spanbert_model.pth")
 async def find_mentions(
         input: dict
 ):
-    print("find_mentions")
-    print("input", input)
 
-    # Find mentions
-    model_output = model.perform_coreference(input['text'])
-
-    # Format output mentions /clusters
-    annotations = process_clusters(input['text'], model_output)
-    
+    annotations = processor.process_live(input['doc'], input['tokens'], model)
 
     return annotations
+
+
+@router.get(
+    "/process_dataset/{id}",
+    summary="Process a dataset",
+    description="Process a dataset"
+)
+async def find_mentions(
+    id: int = Path(..., description="The dataset id"),
+):
+    print("process_dataset")
+
+    await processor.process(id, model)
