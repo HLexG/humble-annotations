3aba8cd3d097f72f4f9560f11fad383dbe0d6371
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-17 07:56:38 -0400

    fe 4 docker network 9013

--- feature-extractor-04/docker-shell.sh
+++ feature-extractor-04/docker-shell.sh
@@ -9,4 +9,4 @@ source ./env.dev
 docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
 
 docker build -t $IMAGE_NAME -f Dockerfile .
-docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9110:9010 -e DEV=1 --network hlexg $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9013:9013 -e DEV=1 --network hlexg $IMAGE_NAME
