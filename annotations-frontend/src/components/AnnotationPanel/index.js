02c8bc879e8ba1fe66904e9d725736181eb0b3b0
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-18 02:23:09 -0400

    Update index.js

--- annotations-frontend/src/components/AnnotationPanel/index.js
+++ annotations-frontend/src/components/AnnotationPanel/index.js
@@ -2,16 +2,42 @@ import React, {useEffect, useRef, useState} from 'react';
 import { withStyles } from '@material-ui/core';
 import InputBase from '@material-ui/core/InputBase';
 import Icon from '@material-ui/core/Icon';
+import DataService from '../../services/DataService';
+import Button from '@material-ui/core/Button';
 
 import styles from './styles';
+
+
+
+
+
 
 const AnnotationPanel = ( props ) => {
     const {classes} = props;
     const { history } = props;
+    const { params } = props;
     let { tokens } = props;
     let { annotations } = props;
+    
 
     console.log("================================== AnnotationPanel ======================================");
+    console.log(props);
+
+    const [id , setId] = useState([]);
+    const [mentions, setMentions] = useState([]);
+    const loadMentions = () => {
+        DataService.GetMentions(5)
+        .then(function (response) {
+            setMentions(response.data)
+            console.log(response.data)
+            //const datasets = setState(response.data[0])
+            // Load the documents
+            //return DataService.GetDocumentsForAnnotation(response.data[0]["id"])
+        })
+        .then(function (response) {
+            //setDocuments(response.data);
+        })
+    }
 
     // Component States
     return (
@@ -29,12 +55,21 @@ const AnnotationPanel = ( props ) => {
                             }}
                         inputProps={{ 'aria-label': 'search' }}
                     />
+                    
                 </div>
+                <Button variant="contained" color="primary" onClick={loadMentions}>
+                Load
+            </Button>
                 <div>
                     <div className={classes.toolstitle}>Mentions:</div>
                     <div>
                     {annotations && annotations["clusters"].map((cluster, index) => 
                         <div key={index}>&nbsp;{cluster.name}</div>
+                    )}
+                    </div>
+                    <div>
+                    {mentions.map(i => 
+                        <div>&nbsp;{i.id}</div>
                     )}
                     </div>
                 </div>
