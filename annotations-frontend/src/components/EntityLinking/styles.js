463c1eb18bbd0a3396da5d5c30988c8652f4686f
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-18 08:04:18 -0400

    icons and styling (later)

---
+++ annotations-frontend/src/components/EntityLinking/styles.js
@@ -0,0 +1,30 @@
+import ChromeReaderModeIcon from '@material-ui/icons/ChromeReaderMode';
+import CreateIcon from '@material-ui/icons/Create';
+import SpellcheckIcon from '@material-ui/icons/Spellcheck';
+import EditLocationIcon from '@material-ui/icons/EditLocation';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import LineWeightIcon from '@material-ui/icons/LineWeight';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import CallMissedIcon from '@material-ui/icons/CallMissed'; //coref
+import SwapCallsIcon from '@material-ui/icons/SwapCalls'; // coref
+import WrapTextIcon from '@material-ui/icons/WrapText'; // coref
+import UndoIcon from '@material-ui/icons/Undo'; //coref or undo
+import FormatClearIcon from '@material-ui/icons/FormatClear'; // adjudicate 
+import GavelIcon from '@material-ui/icons/Gavel'; // adj
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck'; // adh
+import ForwardIcon from '@material-ui/icons/Forward'; // events
+import TransferWithinAStationIcon from '@material-ui/icons/TransferWithinAStation'; // events
+import GroupIcon from '@material-ui/icons/Group'; // entity
+import GetAppIcon from '@material-ui/icons/GetApp'; // download
+import GradientIcon from '@material-ui/icons/Gradient'; // ling diversity
+import HeightIcon from '@material-ui/icons/Height'; // cdcr
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary'; // crowd mgmt
+
+import VerticalAlignCenterIcon from '@material-ui/icons/VerticalAlignCenter'; // group corpora
+
+import ViewCarouselIcon from '@material-ui/icons/ViewCarousel'; // cdcr
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive'; // big corpus
+import DynamicFeedIcon from '@material-ui/icons/DynamicFeed'; // sort for them
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted'; // they pick
+import VerticalSplitIcon from '@material-ui/icons/VerticalSplit'; // cdcr
+
