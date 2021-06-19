fa0992a2e0ed52342f6fcddf98ed32b0615ac654
10686142 <vazz_m@hotmail.com>
2021-06-19 18:24:39 +0200

    frontend: Rfeactors code in DatasetOverview and now initially loading datasets from api

--- annotations-frontend/src/components/DatasetsOverview/index.js
+++ annotations-frontend/src/components/DatasetsOverview/index.js
@@ -1,197 +1,159 @@
 import React, {useEffect, useRef, useState} from 'react';
+import axios from 'axios';
 import { withStyles } from '@material-ui/core';
 import styles from './styles';
 import DatasetCard from './DatasetCard';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import UploadDsCard from './UploadDsCard';
 import DataService from "../../services/DataService";
 
-const testDatasets = [
-  {id: 1, dataset_name: 'CeleBERTy', dataset_description: 'Welcome to learning React! Welcome to learning React! Welcome to learning React!'},
-  {id: 2, dataset_name: 'Apple News', dataset_description: 'You can install React from npm. You can install React from npm. You can install React from npm.'},
-  {id: 3, dataset_name: 'Harry p', dataset_description: 'You can install React from npm. Welcome to learning React! Welcome to learning React!'}
-
-];
-
 const delay = ms => new Promise(res => setTimeout(res, ms));
-const axios = require('axios');
-
-// TODO: Pull out the actual basic info of all the dataset
-const datasets = testDatasets;
-
-const Datasets = ( props ) => {
-    const {classes} = props;
-    const { history } = props;
-    let { tokens } = props;
-    let { annotations } = props;
-
-    const [dialogOpen, setOpen] = useState(false);
-
-    const [formTitle, setFormTitle] = useState("");
-    const [formDescr, setFormDescr] = useState("");
-    const [selectedFormFile, setSelectedFormFile] = useState(null);
-    const [datasets, setDataset] = useState(testDatasets);
-    const [dsetId, setDsetId] = useState(0);
 
 
-     const loadDatasets = () => {
-       DataService.GetDatasets()
-       .then(function (response) {
-           setDataset(response.data)
-           console.log(response.data)
-           //const datasets = setState(response.data[0])
-           // Load the documents
-           //return DataService.GetDocumentsForAnnotation(response.data[0]["id"])
-       })
-       .then(function (response) {
-           //setDocuments(response.data);
-       })
-   }
+const DatasetsOverview = ( props ) => {
+  const {classes} = props;
+
+  // States
+  const [dialogOpen, setOpen] = useState(false);
+  const [formTitle, setFormTitle] = useState("");
+  const [formDescr, setFormDescr] = useState("");
+  const [selectedFormFile, setSelectedFormFile] = useState(null);
+  const [datasets, setDataset] = useState([]);
+  const [dsetId, setDsetId] = useState(0);
+
+  // does the job of componentDidMount, componentDidUpdate, componentWillUpdate combined
+  useEffect(() => {
+    loadDatasets()
+  }, []) // Needed [], else we get an infinite loop to the state changing in loadDatasets()
 
 
-  // https://stackoverflow.com/a/64767180/8970591
-  const convertBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-        resolve(file);
-
-        file.onerror = (error) => {
-        reject(error);
-      }
+  const loadDatasets = () => {
+    DataService.GetDatasets()
+    .then(function (response) {
+        setDataset(response.data)
+        console.log(response.data)
+        //const datasets = setState(response.data[0])
+        // Load the documents
+        //return DataService.GetDocumentsForAnnotation(response.data[0]["id"])
+    })
+    .then(function (response) {
+        //setDocuments(response.data);
     })
   }
 
-    const submitNewDataset = async () => {
-      
-      console.log(formTitle);
-      console.log(formDescr);
-      console.log(selectedFormFile);
+  const submitNewDataset = async () => {
+    // Log popup values for testing
+    console.log(formTitle);
+    console.log(formDescr);
+    console.log(selectedFormFile);
+    
+    // Construct the object for our initial db instance creation
+    const datasetInfo = {
+      dataset_name: formTitle,
+      dataset_description: formDescr
+    }
 
-      const axios = require('axios');
+    // We can now close the dialog
+    handleCloseDialog();
 
-      
+    // Initial dataset creation, needs the title and description
+    DataService.UploadDatasetInfo(datasetInfo)
+    .then(function (response) {
+        console.log(response.data);
+        setDsetId(response.data)
+        console.log(response.data);
 
-      const datasetInfo = {
-        dataset_name: formTitle,
-        dataset_description: formDescr
-      }
+          // Subsequent update to the dataset entry we create to add the actual dataset
+          DataService.UploadDataset(response.data.id, selectedFormFile)
+          .then(function (response) {
+              console.log(response.data);
 
-      DataService.UploadDatasetInfo(datasetInfo)
-      .then(function (response) {
-          console.log(response.data);
-          setDsetId(response.data)
-          console.log(response.data);
+              // Inform the user
+              alert(`Dataset '${formTitle}' uploaded succesfully!`)
 
-
-
-          //convertBase64(selectedFormFile)
-          //.then(fileBase64 => {
-          //  console.log("File Is", fileBase64);
-
-            DataService.UploadDataset(response.data.id, selectedFormFile)
-            .then(function (response) {
-                console.log(response.data);
-
-                
-      
-                
-                // let annotations = response.data;
-      
-            })
-            .then(function (response) {
-;}
-
-
-
-            )
-   
-          
+              // Reload screen to display new ds as well
+              loadDatasets()
+          })
           .catch(err => {
             console.log(err);
+            alert(`Uploading Dataset '${formTitle}' went wrong, please check logs`)
           });
 
-
-          // let annotations = response.data;
-
-      });
-
-      
-
-      
-
-
-      // Lastly close when all correct info has been retrieved
-      // handleCloseDialog();
-    };
-
-    const handleClickOpenDialog = () => {
-      setOpen(true);
-    };
-  
-    const handleCloseDialog = () => {
-      setFormTitle("");
-      setFormDescr("");
-      setSelectedFormFile(null);
-      setOpen(false);
-      loadDatasets()
-    };
-
-    const loadEvents = () => {        
-      axios({
-      method: 'get',
-      url: `http://0.0.0.0:9013/v1/process_dataset_event/${dsetId}`,
-      responseType: 'application/json'
     })
-      .then(function (resp) {
-        console.log('Events = Success!')
-        console.log(resp)
-      });}
+    .catch(err => {
+      console.log(err);
+      alert(`Uploading Dataset '${formTitle}' went wrong, please check logs`)
+    });
+  };
 
-    console.log("================================== DatasetsOverview ======================================");
+  const handleClickOpenDialog = () => {
+    setOpen(true);
+  };
 
-    // Component States
-    return (
-        <div className={classes.root}>
-          <h1 className={classes.headerTextStyle}>Datasets</h1>
-          <p className={classes.headerTextStyle}>Here you can find datasets that you can help to annotate. You can also upload your own dataset so you and your team can start annotating</p>
-          <div className={classes.dsButtons}>
-            <Button variant="contained" color="primary" className={classes.uploadButton} onClick={handleClickOpenDialog}>
-              Upload clean dataset
-            </Button>
-            <Button variant="contained" color="primary" className={classes.uploadButton} onClick={loadDatasets}>
-              refresh
-            </Button>
-            <Button variant="contained" color="primary" className={classes.uploadButton} onClick={loadEvents}>
-              event test
-            </Button>
-            <UploadDsCard 
-              handleClickOpenDialog={handleClickOpenDialog} 
-              handleCloseDialog={handleCloseDialog} 
-              dialogOpen={dialogOpen}
-              formTitle={formTitle}
-              setFormTitle={setFormTitle}
-              formDescr={formDescr}
-              setFormDescr={setFormDescr}
-              setSelectedFormFile={setSelectedFormFile}
-              submitNewDataset={submitNewDataset}
-            />
-            {/* <Button variant="contained" color="secondary">
-              Upload annotated dataset
-            </Button> */}
-          </div>
-          <Grid container spacing={3}>
-          {datasets.map(ds => {
-              return (
-                <Grid item xs={4} key={ds.id}>
-                  <DatasetCard ds={ds}/>
-                </Grid>
-              );
-            })}
-          </Grid>
-          
+  const handleCloseDialog = () => {
+    setFormTitle("");
+    setFormDescr("");
+    setSelectedFormFile(null);
+    setOpen(false);
+    loadDatasets()
+  };
+
+  const loadEvents = () => {        
+    axios({
+    method: 'get',
+    url: `http://0.0.0.0:9013/v1/process_dataset_event/${dsetId}`,
+    responseType: 'application/json'
+  })
+    .then(function (resp) {
+      console.log('Events = Success!')
+      console.log(resp)
+    });}
+
+  console.log("================================== DatasetsOverview ======================================");
+
+  // Component States
+  return (
+      <div className={classes.root}>
+        <h1 className={classes.headerTextStyle}>Datasets</h1>
+        <p className={classes.headerTextStyle}>Here you can find datasets that you can help to annotate. You can also upload your own dataset so you and your team can start annotating</p>
+        <div className={classes.dsButtons}>
+          <Button variant="contained" color="primary" className={classes.uploadButton} onClick={handleClickOpenDialog}>
+            Upload clean dataset
+          </Button>
+          <Button variant="contained" color="primary" className={classes.uploadButton} onClick={loadDatasets}>
+            refresh
+          </Button>
+          <Button variant="contained" color="primary" className={classes.uploadButton} onClick={loadEvents}>
+            event test
+          </Button>
+          <UploadDsCard 
+            handleClickOpenDialog={handleClickOpenDialog} 
+            handleCloseDialog={handleCloseDialog} 
+            dialogOpen={dialogOpen}
+            formTitle={formTitle}
+            setFormTitle={setFormTitle}
+            formDescr={formDescr}
+            setFormDescr={setFormDescr}
+            setSelectedFormFile={setSelectedFormFile}
+            submitNewDataset={submitNewDataset}
+          />
+          {/* <Button variant="contained" color="secondary">
+            Upload annotated dataset
+          </Button> */}
         </div>
-    );
+        <Grid container spacing={3}>
+        {datasets.map(ds => {
+            return (
+              <Grid item xs={4} key={ds.id}>
+                <DatasetCard ds={ds}/>
+              </Grid>
+            );
+          })}
+        </Grid>
+        
+      </div>
+  );
 };
 
-export default withStyles( styles )( Datasets );
+export default withStyles( styles )( DatasetsOverview );
