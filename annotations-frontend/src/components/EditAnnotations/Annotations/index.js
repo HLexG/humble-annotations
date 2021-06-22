fffca7429681edf53139697cf03050a6b340df97
Shivas Jayaram <shivasj@gmail.com>
2021-06-22 08:53:38 -0400

    Added draft version of annotations component

---
+++ annotations-frontend/src/components/EditAnnotations/Annotations/index.js
@@ -0,0 +1,153 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { withStyles } from '@material-ui/core';
+
+import styles from './styles';
+import { buildAnnotationTree } from './functions';
+
+const Annotations = (props) => {
+    const { classes } = props;
+    const { document } = props;
+
+    // Component States
+    const [annotationTree, setAnnotationTree] = useState(null);
+    const loadAnnotationTree = () => {
+        let tree = buildAnnotationTree(document);
+        console.log(tree);
+        setAnnotationTree(tree);
+    }
+    const [selectedToken, setSelectedToken] = useState(null);
+    const [selectedMention, setSelectedMention] = useState(null);
+    const [draggedMention, setDraggedMention] = useState(null);
+    const [refresh, setRefresh] = useState(0);
+
+    const colorList = ["#C0504D", "#1F497D", "#9BBB59", "#F79646", "#4BACC6", "#8064A2", "#948A54", "#C0504D", "#1F497D", "#9BBB59", "#F79646", "#4BACC6", "#8064A2", "#948A54", "#C0504D", "#1F497D", "#9BBB59", "#F79646", "#4BACC6", "#8064A2", "#948A54", "#C0504D", "#1F497D", "#9BBB59", "#F79646", "#4BACC6", "#8064A2", "#948A54", "#C0504D", "#1F497D", "#9BBB59", "#F79646", "#4BACC6", "#8064A2", "#948A54"];
+
+    // Setup Component
+    useEffect(() => {
+        loadAnnotationTree();
+    }, [props.document]);
+
+    // Component functions
+    const isTokenSelected = (token) => {
+        var style = {};
+        var selectedStyle = {
+            textDecoration: "underline"
+        }
+
+        if (selectedToken && (selectedToken.token_id === token.token_id) && (selectedToken.sentence_id === token.sentence_id)) {
+            style = selectedStyle
+        }
+        return style;
+    }
+
+    const isMentionSelected = (mention) => {
+        var style = {};
+        var selectedStyle = {
+            color: "red"
+        }
+
+        if (selectedMention && (selectedMention.id === mention.id) && (selectedMention.sentence_id === mention.sentence_id)) {
+            style = selectedStyle;
+            console.log(mention.id);
+            console.log(mention.sentence_id);
+
+        }
+        return style;
+    }
+
+    // Handlers
+    const handleTokenClick = (event, token, isDouble) => {
+
+    }
+    const handleMentionClick = (event, mention) => {
+        event.stopPropagation();
+        // Select the mention
+        setSelectedMention(mention);
+    };
+
+    const handleKeyDown = (event) => {
+        console.log('Key pressed', event.keyCode);
+        if (event.keyCode === 27) {
+            // Esc
+            setSelectedToken(null);
+        } else if (event.keyCode === 8) {
+            // Delete
+            //annotations.splice(1, 1,);
+        }
+    };
+    const handleMentionDragStart = (event, mention) => {
+        console.log("handleMentionDragStart...", mention);
+        event.stopPropagation();
+        // Select the source mention
+        setDraggedMention(mention);
+    };
+    const handleMentionDragOver = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+    };
+    const handleMentionDrop = (event, mention) => {
+        console.log("handleMentionDrop...", mention);
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    const renderAnnotationItems = (items) => {
+        return (
+            <>
+                {items.map(i => {
+
+                    if (i.type == "token") {
+                        let className = classes.token;
+                        let style = isTokenSelected(i.obj);
+                        let onClick = (event) => handleTokenClick(event, i.obj, false);
+
+                        // Styles for special character tokens
+                        if ([',', '.', ';'].includes(i.obj.token_text)) {
+                            style["whiteSpace"] = "normal";
+                            style["marginLeft"] = "-2px";
+                        }
+                        return (
+                            <a
+                                key={i.id}
+                                className={className}
+                                onClick={onClick}
+                                style={style}
+                                background-color={i.backgroundColor}
+                            >
+                                {i.obj.token_text}
+                            </a>
+                        )
+                    } else {
+                        return (
+                            <span
+                                key={i.id}
+                                className={classes.mention}
+                                draggable={true}
+                                onClick={(event) => handleMentionClick(event, i.obj)}
+                                onDragStart={(event) => handleMentionDragStart(event, i.obj)}
+                                onDragOver={(event) => handleMentionDragOver(event)}
+                                onDrop={(event) => handleMentionDrop(event, i.obj)}
+                                style={isMentionSelected(i.obj)}
+                                background-color={i.backgroundColor}
+                            >
+                                <mark style={{ backgroundColor: colorList[i.obj.cluster_id] }} className={classes.mentionhead}><a className={classes.mentionheadtext}>{i.obj.text}</a></mark>
+                                {i.nodes && renderAnnotationItems(i.nodes)}
+                            </span>
+                        )
+                    }
+
+                })}
+            </>
+        )
+    }
+
+    return (
+        <div className={classes.content}>
+            {annotationTree && annotationTree.nodes && (
+                renderAnnotationItems(annotationTree.nodes)
+            )}
+        </div>
+    );
+};
+
+export default withStyles(styles)(Annotations);
