0e4878e439f9990e7a5b73b1c08524f994f38b39
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-22 09:01:17 -0400

    Update service.py

--- api-service/api/service.py
+++ api-service/api/service.py
@@ -6,7 +6,7 @@ from starlette.staticfiles import StaticFiles
 
 import dataaccess.session as database_session
 from api import auth
-from api.routers import users, datasets, documents, entitylink, mentions, clusters, enums
+from api.routers import users, datasets, documents, entitylink, mentions, clusters, enums, events
 
 
 prefix = "/v1"
@@ -75,3 +75,4 @@ app.include_router(documents.router, prefix=prefix)
 app.include_router(entitylink.router, prefix=prefix)
 app.include_router(mentions.router, prefix=prefix)
 app.include_router(clusters.router, prefix=prefix)
+app.include_router(events.router, prefix=prefix)
