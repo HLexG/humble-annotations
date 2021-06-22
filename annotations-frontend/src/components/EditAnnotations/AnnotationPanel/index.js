8531169e9999e23c0142592eddf169352854ab75
Shivas Jayaram <shivasj@gmail.com>
2021-06-22 08:54:11 -0400

    Placeholder for AnnotationPanle component

---
+++ annotations-frontend/src/components/EditAnnotations/AnnotationPanel/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { withStyles } from '@material-ui/core';
+
+import styles from './styles';
+
+const AnnotationPanel = (props) => {
+    const { classes } = props;
+
+    // Component States
+
+    // Setup Component
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div className={classes.root}>
+
+        </div>
+    );
+};
+
+export default withStyles(styles)(AnnotationPanel);
