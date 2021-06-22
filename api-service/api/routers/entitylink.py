7f4e43dd393848056355b65c143d6a5df476d991
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-22 12:42:51 -0400

    route to pull wikidata entry for user review

--- api-service/api/routers/entitylink.py
+++ api-service/api/routers/entitylink.py
@@ -19,3 +19,12 @@ router = APIRouter()
 async def documents_index(dataset_id: int = Query(None, description="Dataset id to filter by"),
                           text_query: str = Query(None, description="Specific text query")):
     return await entitylink.browse(dataset_id=dataset_id, text_query=text_query)
+
+
+@router.get(
+    "/entitylinks_wd/{id}",
+    summary="Get list of entity links for a dataset",
+    description="Get list of entity links for a dataset"
+)
+async def documents_index(id: str = Query(None, description="Dataset id to filter by")):
+    return await entitylink.get_wd(id=id)
