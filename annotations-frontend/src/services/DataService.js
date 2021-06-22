e89a360b57c133cbc74478b77d968f011181de60
Shivas Jayaram <shivasj@gmail.com>
2021-06-22 08:54:23 -0400

    Added data service for GetDocumentAnnotations

--- annotations-frontend/src/services/DataService.js
+++ annotations-frontend/src/services/DataService.js
@@ -1,5 +1,5 @@
-import {BASE_API_URL} from "./Common";
-import {authHeader} from "./AuthService";
+import { BASE_API_URL } from "./Common";
+import { authHeader } from "./AuthService";
 import PreAnnotation from "./PreAnno";
 
 const axios = require('axios');
@@ -7,39 +7,42 @@ const axios = require('axios');
 
 // TODO: Add the POST endpoints, 2 step process, first title and description and then another POST to upload the zip
 const DataService = {
-    Init: function(){
+    Init: function () {
         // Any application initialization logic comes here
     },
-    GetUseProfile : async function(username){
-        return await axios.get(BASE_API_URL+"/profile/"+username, { headers: authHeader() });
+    GetUseProfile: async function (username) {
+        return await axios.get(BASE_API_URL + "/profile/" + username, { headers: authHeader() });
     },
-    GetDatasets : async function(){
-        return await axios.get(BASE_API_URL+"/datasets", { headers: authHeader() });
+    GetDatasets: async function () {
+        return await axios.get(BASE_API_URL + "/datasets", { headers: authHeader() });
     },
-    UploadDatasetInfo : async function(ds_info){
-        return await axios.post(BASE_API_URL+"/datasets", ds_info, { headers: authHeader() });
+    UploadDatasetInfo: async function (ds_info) {
+        return await axios.post(BASE_API_URL + "/datasets", ds_info, { headers: authHeader() });
     },
-    UploadDataset : async function(dataset_id, ds){
+    UploadDataset: async function (dataset_id, ds) {
         var formData = new FormData();
         formData.append("file", ds);
-        var outputs = await axios.post(BASE_API_URL+"/datasets/"+dataset_id+"/upload", formData, { headers: authHeader() });
+        var outputs = await axios.post(BASE_API_URL + "/datasets/" + dataset_id + "/upload", formData, { headers: authHeader() });
         // Post-upload pre-annotation services go here
         //PreAnnotation.RunSpanBertSpacy(dataset_id)
         //PreAnnotation.RunEventTriggers(dataset_id)
-        
-        return outputs ;
+
+        return outputs;
     },
-    GetDataset : async function(id){
-        return await axios.get(BASE_API_URL+"/datasets/"+id, { headers: authHeader() });
+    GetDataset: async function (id) {
+        return await axios.get(BASE_API_URL + "/datasets/" + id, { headers: authHeader() });
     },
-    GetDocuments : async function(dataset_id){
-        return await axios.get(BASE_API_URL+"/documents?dataset_id="+dataset_id, { headers: authHeader() });
+    GetDocuments: async function (dataset_id) {
+        return await axios.get(BASE_API_URL + "/documents?dataset_id=" + dataset_id, { headers: authHeader() });
     },
-    GetDocument : async function(id){
-        return await axios.get(BASE_API_URL+"/documents/"+id, { headers: authHeader() });
+    GetDocument: async function (id) {
+        return await axios.get(BASE_API_URL + "/documents/" + id, { headers: authHeader() });
     },
-    GetMentions : async function(id){
-        return await axios.get(BASE_API_URL+"/mentionsdoc/"+id, { headers: authHeader() });
+    GetDocumentAnnotations: async function (id, annotation_type) {
+        return await axios.get(BASE_API_URL + "/document/" + id + "/annotations/?annotation_type=" + annotation_type, { headers: authHeader() });
+    },
+    GetMentions: async function (id) {
+        return await axios.get(BASE_API_URL + "/mentionsdoc/" + id, { headers: authHeader() });
     },
 }
 
