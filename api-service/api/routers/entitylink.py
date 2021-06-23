09a5b0124b5ccf1c737449a1ed314ef4ae916b91
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-22 20:27:27 -0400

    routing to sql queries for EL

--- api-service/api/routers/entitylink.py
+++ api-service/api/routers/entitylink.py
@@ -28,3 +28,12 @@ async def documents_index(dataset_id: int = Query(None, description="Dataset id 
 )
 async def documents_index(id: str = Query(None, description="Dataset id to filter by")):
     return await entitylink.get_wd(id=id)
+
+
+@router.get(
+    "/entitylinks_nl/{dataset_id}",
+    summary="Get list of entity links for a dataset",
+    description="Get list of entity links for a dataset"
+)
+async def documents_index(dataset_id: int = Query(None, description="Dataset id to filter by")):
+    return await entitylink.tbnamed(dataset_id=dataset_id)
