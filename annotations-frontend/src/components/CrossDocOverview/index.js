582100d0b352bf274960239b372d46df426a0872
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-22 20:28:24 -0400

    Cross doc overview table start (to allow toggle for entities v. events)

--- annotations-frontend/src/components/CrossDocOverview/index.js
+++ annotations-frontend/src/components/CrossDocOverview/index.js
@@ -6,7 +6,11 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import DataService from '../../services/DataService';
-
+import CollapsibleTable from './CrossDocReviewTable';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import EventIcon from '@material-ui/icons/Event';
+import NaturePeopleIcon from '@material-ui/icons/NaturePeople';
 
 const testDatasets = [
   {id: 1, title: 'CeleBERTy', descr: 'Welcome to learning React! Welcome to learning React! Welcome to learning React!'},
@@ -28,11 +32,17 @@ const CrossDocOverview = ( props ) => {
 
     console.log("================================== CrossDocOverview ======================================");
 
-
+    // PAGE DETAILS
     const [pageTitle, setPageTitle] = useState("CeleBERTy dataset ");
     const [pageDesc, setPageDesc] = useState("Some general info about this dataset");
 
+    //TOGGLE ENTITIES/EVENTS
 
+    const [stateEntityTable, setStateEntityTable] = React.useState(() => ['entities', 'xdocevents']);
+  
+    const handleTableToggle = (event, newTables) => {
+      setStateEntityTable(newTables);
+    };
 
     
 
@@ -49,7 +59,17 @@ const CrossDocOverview = ( props ) => {
         <div className={classes.root}>
           <h1 className={classes.headerTextStyle}>{pageTitle}</h1>
           <p className={classes.headerTextStyle}>{pageDesc}</p>
+          <ToggleButtonGroup value={stateEntityTable} onChange={handleTableToggle} aria-label="text formatting">
+      <ToggleButton value="entities" aria-label="Entities">
+        <NaturePeopleIcon /> Entities
+      </ToggleButton>
+      <ToggleButton value="xdocevents" aria-label="Events" disabled>
+        <EventIcon /> Events
+      </ToggleButton>
+      
+    </ToggleButtonGroup>
           <div className={classes.dsButtons}>
+            <CollapsibleTable/>
             {/* <Button variant="contained" color="secondary">
               Upload annotated dataset
             </Button> */}
