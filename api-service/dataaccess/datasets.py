077e7886ce52c1ca0bf6daf09edd934b334e7cff
Joe Brucker <joe.r.brucker@gmail.com>
2021-06-22 21:08:22 -0400

    name change correct

--- api-service/dataaccess/datasets.py
+++ api-service/dataaccess/datasets.py
@@ -6,7 +6,7 @@ from dataaccess.session import database
 from dataaccess.errors import RecordNotFoundError, NoAccessError
 from dataaccess.types import PermissionType
 from fastapi import APIRouter, Path, Query, Depends, File
-from api.featureExtraction.trigger import preprocesses_entities
+from api.feature_extraction.trigger import preprocesses_entities
 
 
 async def browse(
