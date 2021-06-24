3eecff6a1ddc5c7bce5432ccd397786c93726252
Shivas Jayaram <shivasj@gmail.com>
2021-06-24 07:51:53 -0400

    Added create annotations dataaccess method

--- api-service/dataaccess/annotations.py
+++ api-service/dataaccess/annotations.py
@@ -5,7 +5,7 @@ from dataaccess.types import AnnotationType
 from dataaccess.errors import RecordNotFoundError
 
 
-async def browse_annotations(
+async def browse_user_annotations(
     *,
     document_id: int,
     annotation_type: AnnotationType,
@@ -32,6 +32,42 @@ async def browse_annotations(
     result = await database.fetch_all(query, values)
 
     return [prep_data(row) for row in result]
+
+
+async def create(*,
+                 document_id: int,
+                 user_id: int,
+                 annotation_type: AnnotationType,
+                 id: int = None) -> Dict[str, Any]:
+    """
+    Create a new row. Returns the created record as a dict.
+    """
+
+    # Set the values
+    values = {
+        "document_id": document_id,
+        "user_id": user_id,
+        "type": annotation_type
+    }
+
+    # if the id was passed
+    if id is not None:
+        values["id"] = id
+
+    # Generate the field and values list
+    field_list = ", ".join(values.keys())
+    param_list = ", ".join(":" + key for key in values.keys())
+
+    result = await database.fetch_one(f"""
+        INSERT INTO mentions (
+            {field_list}
+        ) VALUES (
+            {param_list}
+        ) RETURNING *;
+    """, values=values)
+
+    result = prep_data(result)
+    return result
 
 
 def prep_data(result) -> Dict[str, Any]:
