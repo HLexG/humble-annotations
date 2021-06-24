7759cfebe77542077aa4177e881045552f83058a
Shivas Jayaram <shivasj@gmail.com>
2021-06-24 07:52:16 -0400

    Place holders for annotations_copy(...)

--- api-service/api/routers/annotations.py
+++ api-service/api/routers/annotations.py
@@ -2,6 +2,7 @@ import os
 from fastapi import APIRouter, Path, Query
 from starlette.responses import FileResponse
 
+from api.auth import Auth, OptionalAuth
 from dataaccess.types import AnnotationType
 from dataaccess import annotations
 
@@ -19,5 +20,28 @@ async def annotations_index(
     annotation_type: str = Query(
         AnnotationType.entity_mention, description="Annotation type filter"),
 ):
+    return await annotations.browse_user_annotations(document_id=document_id, annotation_type=annotation_type)
 
-    return await annotations.browse_annotations(document_id=document_id, annotation_type=annotation_type)
+
+@router.post(
+    "/annotations/{id}/copy",
+    tags=["Annotations"],
+    summary="Copy annotations",
+    description="Copy annotations",
+    response_description="Status message"
+)
+async def annotations_copy(
+        id: int = Path(..., description="Annotation id to copy from "),
+        annotation_type: str = Query(...,
+                                     description="Annotation type filter"),
+        auth: Auth = Depends()
+):
+    # Check if annotation exists for user for the document
+
+    # Create a new annotation if needed
+
+    # Copy over annotation based on annotation type
+
+    return {
+        "created": True
+    }
