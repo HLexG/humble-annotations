ebcc65f9054eb4d38b563ce862955f9bac1d1959
Shivas Jayaram <shivasj@gmail.com>
2021-06-28 14:36:51 -0400

    Added GCP credentials for feature extractor 3

--- feature-extractor-03/docker-shell.sh
+++ feature-extractor-03/docker-shell.sh
@@ -9,4 +9,12 @@ source ./env.dev
 docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
 
 docker build -t $IMAGE_NAME -f Dockerfile .
-docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9112:9012 -e DEV=1 --network hlexg $IMAGE_NAME
+docker run --rm --name $IMAGE_NAME -ti \
+--mount type=bind,source="$(pwd)",target=/app \
+--mount type=bind,source=$(pwd)/../../secrets/,target=/secrets \
+-p 9112:9012 \
+-e DEV=1 \
+-e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+-e GCP_PROJECT=$GCP_PROJECT \
+-e GCP_ZONE=$GCP_ZONE \
+--network hlexg $IMAGE_NAME
