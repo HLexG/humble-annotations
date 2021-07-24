ed0e518cd28b033ff00ab02bacf7bb5258e61e39
Shivas Jayaram <shivasj@gmail.com>
2021-07-24 18:16:21 -0400

    Added deployment scripts for feature extractors

--- deployment/docker-shell.sh
+++ deployment/docker-shell.sh
@@ -15,6 +15,8 @@ docker run --rm --name $IMAGE_NAME -ti \
 --mount type=bind,source=$(pwd)/../feature-extractor-01,target=/feature-extractor-01 \
 --mount type=bind,source=$(pwd)/../feature-extractor-02,target=/feature-extractor-02 \
 --mount type=bind,source=$(pwd)/../feature-extractor-03,target=/feature-extractor-03 \
+--mount type=bind,source=$(pwd)/../feature-extractor-04,target=/feature-extractor-04 \
+--mount type=bind,source=$(pwd)/../feature-extractor-05,target=/feature-extractor-05 \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCP_ZONE=$GCP_ZONE $IMAGE_NAME
