3f65f14a7e63a5e81a65e7d61970e80eebcb4d2d
10686142 <vazz_m@hotmail.com>
2021-08-26 21:29:33 +0200

    Switched docker container to use amd64, since default doesn't work on M1 Silicon macs. Only couldn't update feature-eactractor-03 since that shell script was different, but that one already works on a M1 mac

--- feature-extractor-02/docker-shell.sh
+++ feature-extractor-02/docker-shell.sh
@@ -8,5 +8,6 @@ source ./env.dev
 # Create the network if we don't have it yet
 docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
 
-docker build -t $IMAGE_NAME -f Dockerfile .
+# docker build -t $IMAGE_NAME -f Dockerfile .
+docker buildx build -t $IMAGE_NAME --platform=linux/amd64 -f Dockerfile .
 docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9111:9011 -e DEV=1 --network hlexg $IMAGE_NAME
--- feature-extractor-04/docker-shell.sh
+++ feature-extractor-04/docker-shell.sh
@@ -8,5 +8,6 @@ source ./env.dev
 # Create network if not done already
 docker network inspect hlexg >/dev/null 2>&1 || docker network create hlexg
 
-docker build -t $IMAGE_NAME -f Dockerfile .
+# docker build -t $IMAGE_NAME -f Dockerfile .
+docker buildx build -t $IMAGE_NAME --platform=linux/amd64 -f Dockerfile .
 docker run --rm --name $IMAGE_NAME -ti --mount type=bind,source="$(pwd)",target=/app -p 9013:9013 -e DEV=1 --network hlexg $IMAGE_NAME
